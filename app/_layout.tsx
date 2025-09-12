ca3039859e14eb290703308fd6256801cedae286
Vasile <vas.lixcode@gmail.com>
2025-09-12 21:26:11 +0300

    Add Rective Native App with router

---
+++ app/_layout.tsx
@@ -0,0 +1,25 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    LatoRegular: require('../assets/Lato-Regular.ttf'),
+    LatoBold: require('../assets/Lato-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hide();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <Stack screenOptions={{header: () => void 0}} />;
+}
